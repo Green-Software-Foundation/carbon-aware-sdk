1422f12d59d42f182e4c848c04def442fa43d774
Vaughan Knight <vaughan@vaughanknight.com>
2021-11-23 11:15:20 +1100

    Missing file in last commit
    Missing file in last commit

--- src/dotnet/CarbonAware.CLI/CarbonAwareCLIState.cs
+++ src/dotnet/CarbonAware.CLI/CarbonAwareCLIState.cs
@@ -7,7 +7,6 @@ namespace CarbonAwareCLI
     public class CarbonAwareCLIState
     {
         public TimeOptionStates TimeOption { get; set; }
-        public OutputOptionStates OutputOption { get; set; }
         public List<string> Locations { get; set; } = new List<string>();
         public DateTime Time { get; set; }
         public bool Lowest { get; set; }
