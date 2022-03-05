b388061449a5c3beff9f83ff15c1189dd55f8b64
Yassine EL GHALI <yelghali@gmail.com>
2022-03-05 15:40:34 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eax
 
-#Exec the CarboneAwareCLI, to return the Region with Lowes Emissions
+#Exec the CarboneAwareCLI, to return the Region with Lowest Emissions
 OutputEmissionsData=$(/CarbonAwareCLI -l $1  -c $2 $3 ) 
 
 #Export the Recommended Region, as a Github Action output, to be used by subsequent workflow steps
