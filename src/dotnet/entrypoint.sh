497e92c4eb78f04252bd096ef96ae5de121c1cd3
Yassine EL GHALI <yelghali@gmail.com>
2022-03-05 14:22:17 +0100

    Update entrypoint.sh

--- src/dotnet/entrypoint.sh
+++ src/dotnet/entrypoint.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 set -eax
 
-/CarbonAwareCLI -l $1  -c $2 $3 
-
-/CarbonAwareCLI -l $1  -c $2 $3 | jq '.Location' 
+OutputEmissionsData=$(/CarbonAwareCLI -l $1  -c $2 $3 | jq '.Location') 
 
 echo "::set-output name=OutputEmissionsData::$(echo $OutputEmissionsData)"
 
