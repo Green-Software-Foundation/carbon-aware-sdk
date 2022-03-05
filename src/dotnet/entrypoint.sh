31fc7520682c83568f1f7e2e90abe9974547adf7
Yassine EL GHALI <yelghali@gmail.com>
2022-03-05 14:42:37 +0100

    Update entrypoint.sh

--- src/dotnet/entrypoint.sh
+++ src/dotnet/entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 set -eax
 
+#Exec the CarboneAwareCLI, to return the Region with Lowes Emissions
 OutputEmissionsData=$(/CarbonAwareCLI -l $1  -c $2 $3 ) 
 
+#Export the Recommended Region, as a Github Action output, to be used by subsequent workflow steps
 echo "::set-output name=LowestEmissionsLocation::$(echo $OutputEmissionsData | jq '.Location')"
 
