ea19018117b6579772b84f2e4e65b1620d6647c5
Bill DeRusha <444835+bderusha@users.noreply.github.com>
2022-03-29 11:57:42 -0400

    Add mock sci-score endpoint

---
+++ src/dotnet/CarbonAware/Model/SciScore.cs
@@ -0,0 +1,18 @@
+﻿namespace CarbonAware.Model;
+
+[Serializable]
+public record SciScore
+{
+    public float SciScoreValue { get; set; }
+    public float EnergyValue { get; set; }
+    public float MarginalCarbonEmissionsValue { get; set; }
+    public float EmbodiedEmissionsValue { get; set; }
+    public Int64 FunctionalUnitValue { get; set; }
+}
+
+[Serializable]
+public record SciScoreCalculation
+{
+    public string AzRegion { get; set; }
+    public string Duration { get; set; }
+}
