c8dcbbb20f08bdd38f255344dc68ac4e305cdf68
Juan Zuluaga <juzuluag@microsoft.com>
2022-03-30 08:22:27 -0400

    Fix tabs

--- src/dotnet/CarbonAware.WebApi/Configuration/CarbonAwareConfigurationExtension.cs
+++ src/dotnet/CarbonAware.WebApi/Configuration/CarbonAwareConfigurationExtension.cs
@@ -1,21 +1,20 @@
-
 using CarbonAware.Plugins.NewBasicJsonPlugin;
 
 namespace CarbonAware.WebApi.Configuration;
 
 public static class CarbonAwareServicesConfiguration
 {
-	public static void AddCarbonAwareServices(this IServiceCollection services, IConfiguration configuration)
-	{
-		var section = configuration.GetSection("ConfigDataSection");
-		var pluginType = section.GetValue<string>("PLUGIN");
-		if (pluginType == "WattTime")
-		{
-			services.AddHttpClient();
-			// services.AddSingleton<ICarbonAware, CarbonAwareWattTime>();
-			// services.AddSingleton<IRestClientWattTime, RestClientWattTime>();
-		} else {
-			services.AddSingleton<ICarbonAware, CarbonAwareNewBasicJsonPlugin>();
-		}
-	}
+    public static void AddCarbonAwareServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        var section = configuration.GetSection("ConfigDataSection");
+        var pluginType = section.GetValue<string>("PLUGIN");
+        if (pluginType == "WattTime")
+        {
+            services.AddHttpClient();
+            // services.AddSingleton<ICarbonAware, CarbonAwareWattTime>();
+            // services.AddSingleton<IRestClientWattTime, RestClientWattTime>();
+        } else {
+            services.AddSingleton<ICarbonAware, CarbonAwareNewBasicJsonPlugin>();
+        }
+    }
 }
--- src/dotnet/CarbonAware/Model/QueryObject.cs
+++ src/dotnet/CarbonAware/Model/QueryObject.cs
@@ -1,10 +1,9 @@
-namespace CarbonAware.Model
-{
-    public class QueryObject
-    {
-        public string Location { get; set; }
-        public DateTime Time { get; set; }
-        public double Rating { get; set; }
+namespace CarbonAware.Model;
 
-    }
+public class QueryObject
+{
+    public string Location { get; set; }
+    public DateTime Time { get; set; }
+    public double Rating { get; set; }
+
 }
