0cf2092781fd8db0857720463a4a8f8027a0b6ab
Juan Zuluaga <juzuluag@microsoft.com>
2022-04-04 18:16:38 -0400

    Remove unused class

--- src/dotnet/CarbonAware/Model/QueryObject.cs
+++
@@ -1,9 +0,0 @@
-namespace CarbonAware.Model;
-
-public class QueryObject
-{
-    public string Location { get; set; }
-    public DateTime Time { get; set; }
-    public double Rating { get; set; }
-
-}
