c8decb342163c61fc526774b0209133f30ada1bc
Jennifer Madiedo <jmadiedo@outlook.com>
2022-04-04 12:27:43 -0400

    PR comment

--- src/dotnet/CarbonAware.WebApi.Tests/unitTests/CarbonAwareControllerTests.cs
+++ src/dotnet/CarbonAware.WebApi.Tests/unitTests/CarbonAwareControllerTests.cs
@@ -1,89 +1,88 @@
-namespace CarbonAware.WepApi.UnitTests
-{
-    using System.Collections.Generic;
-    using CarbonAware.Model;
-    using CarbonAware.WebApi.Controllers;
-    using Microsoft.AspNetCore.Mvc;
-    using NUnit.Framework;
+namespace CarbonAware.WepApi.UnitTests;
+
+using System.Collections.Generic;
+using CarbonAware.Model;
+using CarbonAware.WebApi.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+
+/// <summary>
+/// Testing Web API Controller
+/// </summary>
+[TestFixture]
+public class CarbonAwareControllerTests : TestsBase
+{   
+    private CarbonAwareController? controller;
+
+    [SetUp]
+    public void SetUp()
+    {
+        this.controller = new CarbonAwareController(this.MockLogger.Object, this.MockPlugin.Object);
+    }
 
     /// <summary>
-    /// Testing Web API Controller
+    /// Tests that successfull call to plugin results in action with 200 status
     /// </summary>
-    [TestFixture]
-    public class CarbonAwareControllerTests : TestsBase
-    {   
-        private CarbonAwareController? controller;
-
-        [SetUp]
-        public void SetUp()
+    [Test]
+    public async Task SuccessfulCallReturnsOk()
+    {
+        string location = "Sydney";
+        var data = new List<EmissionsData>()
         {
-            this.controller = new CarbonAwareController(this.MockLogger.Object, this.MockPlugin.Object);
-        }
-
-        /// <summary>
-        /// Tests that successfull call to plugin results in action with 200 status
-        /// </summary>
-        [Test]
-        public async Task SuccessfulCallReturnsOk()
-        {
-            string location = "Sydney";
-            var data = new List<EmissionsData>()
+            new EmissionsData()
             {
-                new EmissionsData()
-                {
-                    Location = location,
-                    Rating = 0.9,
-                    Time = DateTime.Now
-                }
-            };
-            this.SetupPluginWithData(data);
+                Location = location,
+                Rating = 0.9,
+                Time = DateTime.Now
+            }
+        };
+        this.SetupPluginWithData(data);
             
 
-            IActionResult ar1 = await this.controller!.GetEmissionsDataForLocationByTime(location);
-            IActionResult ar2 = await this.controller!.GetBestEmissionsDataForLocationsByTime(new string[] { location });
-            IActionResult ar3 = await this.controller!.GetEmissionsDataForLocationsByTime(new string[] { location });
+        IActionResult ar1 = await this.controller!.GetEmissionsDataForLocationByTime(location);
+        IActionResult ar2 = await this.controller!.GetBestEmissionsDataForLocationsByTime(new string[] { location });
+        IActionResult ar3 = await this.controller!.GetEmissionsDataForLocationsByTime(new string[] { location });
 
-            TestHelpers.AssertStatusCode(ar1, 200);
-            TestHelpers.AssertStatusCode(ar2, 200);
-            TestHelpers.AssertStatusCode(ar3, 200);
-        }
+        TestHelpers.AssertStatusCode(ar1, 200);
+        TestHelpers.AssertStatusCode(ar2, 200);
+        TestHelpers.AssertStatusCode(ar3, 200);
+    }
 
-        /// <summary>
-        /// Tests that empty result from plugin results in action with 204 status
-        /// </summary>
-        [Test]
-        public async Task EmptyResultRetuns204()
-        {
-            this.SetupPluginWithData(new List<EmissionsData>());
-            string location = "Sydney";
+    /// <summary>
+    /// Tests that empty result from plugin results in action with 204 status
+    /// </summary>
+    [Test]
+    public async Task EmptyResultRetuns204()
+    {
+        this.SetupPluginWithData(new List<EmissionsData>());
+        string location = "Sydney";
 
-            IActionResult ar1 = await this.controller!.GetEmissionsDataForLocationByTime(location);
-            IActionResult ar2 = await this.controller!.GetBestEmissionsDataForLocationsByTime(new string[] {location});
-            IActionResult ar3 = await this.controller!.GetEmissionsDataForLocationsByTime(new string[] { location });
+        IActionResult ar1 = await this.controller!.GetEmissionsDataForLocationByTime(location);
+        IActionResult ar2 = await this.controller!.GetBestEmissionsDataForLocationsByTime(new string[] {location});
+        IActionResult ar3 = await this.controller!.GetEmissionsDataForLocationsByTime(new string[] { location });
 
-            //Assert
-            TestHelpers.AssertStatusCode(ar1, 204);
-            TestHelpers.AssertStatusCode(ar2, 204);
-            TestHelpers.AssertStatusCode(ar3, 204);
-        }
+        //Assert
+        TestHelpers.AssertStatusCode(ar1, 204);
+        TestHelpers.AssertStatusCode(ar2, 204);
+        TestHelpers.AssertStatusCode(ar3, 204);
+    }
 
-        /// <summary>
-        /// Tests that exception in plugin results in action with 400 status
-        /// </summary>
-        [Test]
-        public async Task ExceptionReturns400()
-        {
-            this.SetupPluginWithException();
-            string location = "Sydney";
+    /// <summary>
+    /// Tests that exception in plugin results in action with 400 status
+    /// </summary>
+    [Test]
+    public async Task ExceptionReturns400()
+    {
+        this.SetupPluginWithException();
+        string location = "Sydney";
 
-            IActionResult ar1 = await this.controller!.GetEmissionsDataForLocationByTime(location);
-            IActionResult ar2 = await this.controller!.GetBestEmissionsDataForLocationsByTime(new string[] { location });
-            IActionResult ar3 = await this.controller!.GetEmissionsDataForLocationsByTime(new string[] { location });
+        IActionResult ar1 = await this.controller!.GetEmissionsDataForLocationByTime(location);
+        IActionResult ar2 = await this.controller!.GetBestEmissionsDataForLocationsByTime(new string[] { location });
+        IActionResult ar3 = await this.controller!.GetEmissionsDataForLocationsByTime(new string[] { location });
 
-            // Assert
-            TestHelpers.AssertStatusCode(ar1, 400);
-            TestHelpers.AssertStatusCode(ar2, 400);
-            TestHelpers.AssertStatusCode(ar3, 400);
-        }
+        // Assert
+        TestHelpers.AssertStatusCode(ar1, 400);
+        TestHelpers.AssertStatusCode(ar2, 400);
+        TestHelpers.AssertStatusCode(ar3, 400);
     }
 }
--- src/dotnet/CarbonAware.WebApi.Tests/unitTests/TestsBase.cs
+++ src/dotnet/CarbonAware.WebApi.Tests/unitTests/TestsBase.cs
@@ -1,32 +1,31 @@
-namespace CarbonAware.WepApi.UnitTests
+namespace CarbonAware.WepApi.UnitTests;
+
+using CarbonAware.Model;
+using CarbonAware.WebApi.Controllers;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+/// <summary>
+/// TestsBase for all WebAPI specific tests.
+/// </summary>
+public abstract class TestsBase
 {
-    using CarbonAware.Model;
-    using CarbonAware.WebApi.Controllers;
-    using Microsoft.Extensions.Logging;
-    using Moq;
-
-    /// <summary>
-    /// TestsBase for all WebAPI specific tests.
-    /// </summary>
-    public abstract class TestsBase
+    protected TestsBase()
     {
-        protected TestsBase()
-        {
-            this.MockLogger = new Mock<ILogger<CarbonAwareController>>();
-            this.MockPlugin = new Mock<ICarbonAware>();
-        }
-
-        protected Mock<ILogger<CarbonAwareController>> MockLogger { get; }
-        protected Mock<ICarbonAware> MockPlugin { get; }
-
-        protected void SetupPluginWithData(List<EmissionsData> data) =>
-            this.MockPlugin.Setup(x =>
-                x.GetEmissionsDataAsync(
-                    It.IsAny<Dictionary<string, object>>())).ReturnsAsync(data);
-
-        protected void SetupPluginWithException() =>
-            this.MockPlugin.Setup(x =>
-                x.GetEmissionsDataAsync(
-                    It.IsAny<Dictionary<string, object>>())).Throws<Exception>();
+        this.MockLogger = new Mock<ILogger<CarbonAwareController>>();
+        this.MockPlugin = new Mock<ICarbonAware>();
     }
+
+    protected Mock<ILogger<CarbonAwareController>> MockLogger { get; }
+    protected Mock<ICarbonAware> MockPlugin { get; }
+
+    protected void SetupPluginWithData(List<EmissionsData> data) =>
+        this.MockPlugin.Setup(x =>
+            x.GetEmissionsDataAsync(
+                It.IsAny<Dictionary<string, object>>())).ReturnsAsync(data);
+
+    protected void SetupPluginWithException() =>
+        this.MockPlugin.Setup(x =>
+            x.GetEmissionsDataAsync(
+                It.IsAny<Dictionary<string, object>>())).Throws<Exception>();
 }
