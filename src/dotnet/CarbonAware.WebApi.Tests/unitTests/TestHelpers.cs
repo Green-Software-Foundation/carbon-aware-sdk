259cdc68a331a492d789c3f1a8024340fc2f4267
Jennifer Madiedo <jmadiedo@outlook.com>
2022-04-04 11:15:39 -0400

    missed save

--- src/dotnet/CarbonAware.WebApi.Tests/unitTests/TestHelpers.cs
+++ src/dotnet/CarbonAware.WebApi.Tests/unitTests/TestHelpers.cs
@@ -12,6 +12,8 @@ namespace CarbonAware.WepApi.UnitTests
         {
             if (result is not ObjectResult obj)
             {
+                // Certain result, like NoContent(), will only return a StatusCodeResult
+                // object instead of the full ObjectResult so we handle here.
                 var statusCodeResult = result as StatusCodeResult;
                 Assert.IsNotNull(statusCodeResult);
                 Assert.IsTrue(statusCodeResult!.StatusCode == code);
