49f4d08e7e702620693eb667acdbd182db49d8b0
KanishkT123 <kanishk357@gmail.com>
2022-04-18 11:52:11 -0400

    upstream branch traacking

--- ms-internal/git-merge.sh
+++ ms-internal/git-merge.sh
@@ -5,23 +5,21 @@ upstreamRepo="https://github.com/Green-Software-Foundation/carbon-aware-sdk"
 git config user.name "GitHub Actions Bot"
 git config user.email "<>"
 
-git checkout -b $1
-
 git remote add upstream $upstreamRepo
 git fetch upstream
-git merge upstream/dev --allow-unrelated-histories
+git checkout -b upstream-dev-$1 upstream/dev
+git push  --set-upstream origin upstream-dev-$1
+gh pr create -f
 
-status=$?
-
-if [ $status -eq 0 ]; then
-    echo "No merge conflicts. Opening PR against the new branch."
-    git push --set-upstream origin upstream-pr-${GITHUB_RUN_ID}
-    gh pr create --title "Pull request title" --body "Pull request body"
-    exit 0
-else
-    echo "Merge Conflicts are preventing auto-merging."
-    git merge --abort
-    git checkout upstream/dev
-    gp pr create -f
-    exit 0
-fi
+# if [ $status -eq 0 ]; then
+#     echo "No merge conflicts. Opening PR against the new branch."
+#     git push --set-upstream origin upstream-pr-${GITHUB_RUN_ID}
+#     gh pr create --title "Pull request title" --body "Pull request body"
+#     exit 0
+# else
+#     echo "Merge Conflicts are preventing auto-merging."
+#     git merge --abort
+#     git checkout upstream/dev
+#     gh pr create -f
+#     exit 0
+# fi
