4efcce77eb8a19e1c477f508fb9848f9a712f31c
KanishkT123 <kanishk357@gmail.com>
2022-04-18 12:32:18 -0400

    vanilla command

--- ms-internal/git-merge.sh
+++ ms-internal/git-merge.sh
@@ -12,9 +12,10 @@ git checkout -b $1
 git push -u origin $1
 
 gh auth login
-gh repo sync --source $upstreamRepo --branch $1
+# gh repo sync --source $upstreamRepo --branch $1
+gh repo sync
 
-gh pr create --title "test" --body "test" --repo microsoft/carbon-aware-sdk
+# gh pr create --title "test" --body "test" --repo microsoft/carbon-aware-sdk
 
 # if [ $status -eq 0 ]; then
 #     echo "No merge conflicts. Opening PR against the new branch."
