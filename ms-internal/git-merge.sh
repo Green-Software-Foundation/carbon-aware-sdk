1a3bb0cd3f738bf2c0548b78fcaf4c773f702e65
Dan Balma <dbalma@microsoft.com>
2022-04-25 18:23:22 -0500

    more cleanup and use params from yml

--- ms-internal/git-merge.sh
+++ ms-internal/git-merge.sh
@@ -1,45 +1,48 @@
 #!/bin/bash
 
-localRepo="https://github.com/microsoft/carbon-aware-sdk"
-upstreamRepo="https://github.com/Green-Software-Foundation/carbon-aware-sdk"
-commitmessage="Warning: git push of upstream contents failed, saving error info into an empty-PR for investigation"
-status=0
+BRANCH=${1:-"auto-pr-$RANDOM"}
+BASE=${2:-"dev"}
+UPSTREAM=${3:-"Green-Software-Foundation/carbon-aware-sdk"}
+
+UPSTREAMREPO="https://github.com/$UPSTREAM"}
+COMMITMESSAGE="Warning: git push of upstream contents failed, saving error info into an empty-PR for investigation"
+STATUS=0
 
 git config user.name "GitHub Actions Bot"
 git config user.email "<>"
 
 # upstream contents needed for creating the PR
-git remote add upstream $upstreamRepo
+git remote add upstream $UPSTREAMREPO
 git fetch upstream
-git checkout -b $1 upstream/dev 
+git checkout -b $BRANCH upstream/$BASE 
 
 # git push, capturing output text and exit code
-echo "Creating a PR with fetched-contents from upstream:dev into origin:dev."
-GIT_PUSH_OUTPUT=$(git push --set-upstream origin $1 2>&1) # without 'workflow' permission this fails if changes to .github\workflow\*.yml file(s) are present
-status=$?
+echo "Creating a PR with fetched-contents from upstream:$BASE into origin:$BASE."
+GIT_PUSH_OUTPUT=$(git push --set-upstream origin $BRANCH 2>&1) # without 'workflow' permission this fails if changes to .github\workflow\*.yml file(s) are present
+STATUS=$?
 echo $GIT_PUSH_OUTPUT
 
 # 'Failure-to-Launch' if git push fails -- create and empty-PR with the error details for manual resolution
-if [ $status -ne 0 ]; then
-    echo $commitmessage
+if [ $STATUS -ne 0 ]; then
+    echo $COMMITMESSAGE
 
     # 1. re-create branch from origin:dev
-    git checkout -f dev
-    git branch -D $1
+    git checkout -f $BASE
+    git branch -D $BRANCH
     git fetch origin
-    git checkout -b $1 origin/dev
+    git checkout -b $BRANCH origin/$BASE
 
     # 2. add + commit the error details
-    echo $commitmessage > README-$1.md
-    echo "-------------" >> README-$1.md
-    echo $GIT_PUSH_OUTPUT >> README-$1.md
-    git add README-$1.md
-    git commit -m "$commitmessage"
+    echo $COMMITMESSAGE > README-$BRANCH.md
+    echo "-------------" >> README-$BRANCH.md
+    echo $GIT_PUSH_OUTPUT >> README-$BRANCH.md
+    git add README-$BRANCH.md
+    git commit -m "$COMMITMESSAGE"
 
     # 3. push to origin
-    git push --set-upstream origin $1         
-    status=$?
+    git push --set-upstream origin $BRANCH         
+    STATUS=$?
 fi
 
 # tell next pipeline step if push failed
-exit $status
+exit $STATUS
