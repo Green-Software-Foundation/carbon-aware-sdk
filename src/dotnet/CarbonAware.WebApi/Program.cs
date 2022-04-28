544d0ee3b6c7bd83fb3eee1b38d6aff6a14c9640
Kanishk Tantia <kanishk357@gmail.com>
2022-04-28 10:34:12 -0400

    Basic healthcheck (#35)
    * Basic healthcheck
    
    * Update build-webapi.yaml

--- src/dotnet/CarbonAware.WebApi/Program.cs
+++ src/dotnet/CarbonAware.WebApi/Program.cs
@@ -11,6 +11,8 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddCarbonAwareEmissionServices();
 
+builder.Services.AddHealthChecks();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -25,5 +27,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapHealthChecks("/health");
 
 app.Run();
