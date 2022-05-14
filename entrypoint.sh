5acf495286690a62ccdd67f01dff4b716372c472
Yassine EL GHALI <yelghali@gmail.com>
2022-05-14 03:34:47 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,3 +12,19 @@ OutputEmissionsData=$(/CarbonAwareCLI -l $1  -c $2 $3 )
 echo "::set-output name=LowestEmissionsLocation::$(echo $OutputEmissionsData | jq '.[0].Location')"
 
 echo "::set-output name=LowestEmissionsTime::$(echo $OutputEmissionsData | jq '.[0].Time')"
+
+TimeOutput=$(echo $OutputEmissionsData | jq '.[0].Time')
+
+#Parse Time String to Convert to CronExpression
+hour_minute_sec="${TimeOutput#*T}"
+hour=$(cut -d : -f 1 <<< $hour_minute_sec)
+minute=$(cut -d : -f 2 <<< $hour_minute_sec)
+
+
+day_month_year="${TimeOutput%T*}"
+month=$(cut -d - -f 2 <<< $day_month_year)
+day=$(cut -d - -f 3 <<< $day_month_year)
+
+TimeCron="$minute $hour $day $month *"
+
+echo "::set-output name=LowestEmissionsTimeCron::$TimeCron"
