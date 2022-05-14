cf23d85b4b73ab812a494cd6fd40bbda0e1e1975
Yassine EL Ghali <yelghali@gmail.com>
2022-05-14 02:34:34 +0200

    adding 'Time' as an output for SDK Github Action

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,3 +10,5 @@ OutputEmissionsData=$(/CarbonAwareCLI -l $1  -c $2 $3 )
 #Export the Recommended Region, as a Github Action output, to be used by subsequent workflow steps
 # the CLI might return several Regions, for the current version of the Github Action, we return one of the lowest
 echo "::set-output name=LowestEmissionsLocation::$(echo $OutputEmissionsData | jq '.[0].Location')"
+
+echo "::set-output name=LowestEmissionsTime::$(echo $OutputEmissionsData | jq '.[0].Time')"
