2968cfd93c5b16b82b3d5be3aad85df2fcb23a95
Szymon Duchniewicz <szymon.duchniewicz@avanade.com>
2022-08-17 12:05:41 +0100

    WIP: Add Time-shifting plotting.
    Signed-off-by: Szymon Duchniewicz <szymon.duchniewicz@avanade.com>

--- src/clients/python/test_api.py
+++ src/clients/python/test_api.py
@@ -12,20 +12,16 @@ configuration = openapi_client.Configuration(
 )
 
 
+api_client= openapi_client.ApiClient(configuration)
+api_instance = carbon_aware_api.CarbonAwareApi(api_client) 
+location = "eastus" # str |  (optional)
+time = parse('2022-07-22T10:30:00.00Z') # datetime |  (optional)
+to_time = parse('2022-07-22T11:00:00.00Z') # datetime |  (optional)
+duration_minutes = 0 # int |  (optional) (default to 0)
 
-# Enter a context with an instance of the API client
-with openapi_client.ApiClient(configuration) as api_client:
-    # Create an instance of the API class
-    api_instance = carbon_aware_api.CarbonAwareApi(api_client)
-    location = "westus" # str |  (optional)
-    time = parse('2022-07-22T10:30:00.00Z') # datetime |  (optional)
-    to_time = parse('2022-07-22T11:00:00.00Z') # datetime |  (optional)
-    duration_minutes = 0 # int |  (optional) (default to 0)
+try:
+    api_response = api_instance.get_emissions_data_for_location_by_time(location=location, time=time, to_time=to_time, duration_minutes=duration_minutes)
 
-    try:
-        api_response = api_instance.get_emissions_data_for_location_by_time(location=location, time=time, to_time=to_time, duration_minutes=duration_minutes)
-
-        pprint(api_response)
-    except openapi_client.ApiException as e:
-        print("Exception when calling CarbonAwareApi->emissions_bylocation_get: %s\n" % e)
-
+    pprint(api_response)
+except openapi_client.ApiException as e:
+    print("Exception when calling CarbonAwareApi->emissions_bylocation_get: %s\n" % e)
