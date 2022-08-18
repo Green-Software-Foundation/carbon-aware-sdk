8fae45ee8e7771bdaf85897cefadfa32b4f9482e
Dylan Hall <dylan.hall077@gmail.com>
2022-08-18 12:06:32 +1200

    Fixing output to return a JSON, and adding simple test for validating

--- src/CarbonAware.CLI/test/CarbonAwareCLITests.cs
+++ src/CarbonAware.CLI/test/CarbonAwareCLITests.cs
@@ -4,6 +4,11 @@ using System;
 using System.IO;
 using CarbonAware.Aggregators.CarbonAware;
 using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using System.Collections;
+using CarbonAware.Model;
+using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace CarbonAware.CLI.Tests;
 
@@ -28,6 +33,34 @@ public class CarbonAwareCLITests
         
         Assert.AreEqual("test", cli._state.Locations[0]); 
         Assert.AreEqual(DateTime.Parse("2021-11-11"), cli._state.Time); 
+    }
+
+    [Test]
+    public void ParseCommandLineArguments_ReturnsJsonWithArrayInside()
+    {
+        string[] args = new string[] { "-l", "test", "-t", "2021-11-11", "--lowest" };
+
+        EmissionsData exampleEmissionsData = new EmissionsData()
+        {
+            Time = DateTime.Now + TimeSpan.FromHours(-1),
+            Location = "US",
+            Rating = 100
+        };
+
+        var cli = new CarbonAwareCLI(args, It.IsAny<ICarbonAwareAggregator>(), Mock.Of<ILogger<CarbonAwareCLI>>());
+
+        using (StringWriter writer = new StringWriter())
+        {
+            // Redirect Console.Out so we can verify what is written
+            Console.SetOut(writer);
+
+            cli.OutputEmissionsData(new List<EmissionsData> { exampleEmissionsData, exampleEmissionsData });
+
+            // Verify that output is in a JSON format. For now, just checking the first character
+            // TODO: Expand out to full verification (other tests + helper methods)
+            string jsonOutput = writer.ToString();
+            Assert.IsTrue(jsonOutput.StartsWith("{"));
+        }
     }
 
     [Test]
