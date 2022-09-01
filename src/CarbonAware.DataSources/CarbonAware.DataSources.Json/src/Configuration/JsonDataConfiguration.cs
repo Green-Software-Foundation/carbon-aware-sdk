b07fbe2be903f06239391fe3b8f2a4ca0ad7a408
Juan Zuluaga <juzuluag@microsoft.com>
2022-09-01 13:54:07 -0400

    Add dir name validation

--- src/CarbonAware.DataSources/CarbonAware.DataSources.Json/src/Configuration/JsonDataConfiguration.cs
+++ src/CarbonAware.DataSources/CarbonAware.DataSources.Json/src/Configuration/JsonDataConfiguration.cs
@@ -1,3 +1,6 @@
+using System.Reflection;
+using System.Text.RegularExpressions;
+
 namespace CarbonAware.DataSources.Json.Configuration;
 
 /// <summary>
@@ -5,18 +8,43 @@ namespace CarbonAware.DataSources.Json.Configuration;
 /// </summary>
 public class JsonDataConfiguration
 {
-    private const string BaseDir = "../data/data-files/";
+    private const string BaseDir = "data-files";
     private const string DefaultDataFile = "test-data-azure-emissions.json";
-    private string dataFileLocation = Path.Combine(BaseDir, DefaultDataFile);
+    private const string RegExDir = "[^a-zA-Z0-9_]+";
+    private string? dataFileLocation;
+    private string assemblyDirectory;
 
     public const string Key = "JsonData";
+
+
+    public JsonDataConfiguration()
+    {
+        var assemblyPath = Assembly.GetExecutingAssembly().Location;
+        assemblyDirectory = Path.GetDirectoryName(assemblyPath)!;
+        DataFileLocation = DefaultDataFile;
+    }
 
     /// <summary>
     /// Json data file location
     /// </summary>
     public string DataFileLocation
     {
-        get => dataFileLocation;
-        set => dataFileLocation = Path.Combine(BaseDir, value);
+        get => dataFileLocation!;
+        set
+        {
+            if (!IsValidDirPath(value))
+            {
+                throw new ArgumentException("File path {value} contains invalid characters.", value);
+            }
+            dataFileLocation = Path.Combine(assemblyDirectory, BaseDir, value);
+        }
+    }
+
+    private static bool IsValidDirPath(string fileName)
+    {
+        var dirName = Path.GetDirectoryName(fileName);
+        var rgex = new Regex(RegExDir);
+        var matched = rgex.Matches(dirName!);
+        return matched.Count == 0;
     }
 }
