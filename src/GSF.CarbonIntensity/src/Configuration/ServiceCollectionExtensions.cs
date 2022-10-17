b09984fc81901cfe188f9cb91504f3d44f00b664
Jennifer Madiedo <jmadiedo@outlook.com>
2022-10-17 14:35:14 -0400

    Adding route for emissionsHandler

---
+++ src/GSF.CarbonIntensity/src/Configuration/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+using CarbonAware.Aggregators.Configuration;
+using GSF.CarbonIntensity.Handlers;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace GSF.CarbonIntensity.Configuration;
+
+public static class ServiceCollectionExtensions
+{
+    /// <summary>
+    /// Add services needed in order to use an CarbonIntensity service.
+    /// </summary>
+    public static IServiceCollection AddCarbonIntensityServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddCarbonAwareEmissionServices(configuration);
+        services.TryAddSingleton<IEmissionsHandler, EmissionsHandler>();
+        return services;
+    }
+}
