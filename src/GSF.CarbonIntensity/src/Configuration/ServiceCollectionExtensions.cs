9b3dad0eb1654812825dea5e639f18ec31f4752b
Juan Zuluaga <juzuluag@microsoft.com>
2022-10-18 15:55:38 -0400

    Decouple services into independent registration calls

--- src/GSF.CarbonIntensity/src/Configuration/ServiceCollectionExtensions.cs
+++ src/GSF.CarbonIntensity/src/Configuration/ServiceCollectionExtensions.cs
@@ -9,12 +9,21 @@ namespace GSF.CarbonIntensity.Configuration;
 public static class ServiceCollectionExtensions
 {
     /// <summary>
-    /// Add services needed in order to use an CarbonIntensity service.
+    /// Add services needed in order to use an Emissions service.
     /// </summary>
-    public static IServiceCollection AddCarbonIntensityServices(this IServiceCollection services, IConfiguration configuration)
+    public static IServiceCollection AddEmissionsServices(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddCarbonAwareEmissionServices(configuration);
         services.TryAddSingleton<IEmissionsHandler, EmissionsHandler>();
+        return services;
+    }
+
+    /// <summary>
+    /// Add services needed in order to use an Forecast service.
+    /// </summary>
+    public static IServiceCollection AddForecastServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddCarbonAwareEmissionServices(configuration);
         services.TryAddSingleton<IForecastHandler, ForecastHandler>();
         return services;
     }
