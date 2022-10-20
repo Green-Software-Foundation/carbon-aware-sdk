dde6b5af41c2ae34b9eda4894130559a50332304
Bill DeRusha <444835+bderusha@users.noreply.github.com>
2022-10-20 16:12:03 -0400

    Remove existing CLI code

--- src/CarbonAware.CLI/src/Usings.cs
+++
@@ -1,7 +0,0 @@
-﻿global using CarbonAware;
-global using CarbonAware.Config;
-global using CarbonAware.Model;
-global using CarbonAwareCLI.Options;
-global using CommandLine;
-global using Microsoft.Extensions.DependencyInjection;
-global using Newtonsoft.Json;
