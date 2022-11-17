aeac19610a9c156c213c2171fb8922f500220f68
Bill DeRusha <444835+bderusha@users.noreply.github.com>
2022-11-17 16:06:50 -0500

    BugFix for JSON integration test data

--- src/CarbonAware.DataSources/CarbonAware.DataSources.Json/mock/JsonDataSourceMocker.cs
+++ src/CarbonAware.DataSources/CarbonAware.DataSources.Json/mock/JsonDataSourceMocker.cs
@@ -1,11 +1,42 @@
-﻿using CarbonAware.DataSources.Mocks;
+﻿using CarbonAware.DataSources.Json.Configuration;
+using CarbonAware.DataSources.Mocks;
+using CarbonAware.Model;
+using System.Text.Json;
 
 namespace CarbonAware.DataSources.Json.Mocks;
 public class JsonDataSourceMocker : IDataSourceMocker
 {
     public JsonDataSourceMocker() { }
 
-    public void SetupDataMock(DateTimeOffset start, DateTimeOffset end, string location) { }
+    public void SetupDataMock(DateTimeOffset start, DateTimeOffset end, string location)
+    {
+        string path = new JsonDataSourceConfiguration().DataFileLocation;
+
+        var data = new List<EmissionsData>();
+        DateTimeOffset pointTime = start;
+        TimeSpan duration = TimeSpan.FromHours(8);
+
+        while (pointTime < end)
+        {
+            var newDataPoint = new EmissionsData()
+            {
+                Location = location,
+                Time = pointTime,
+                Rating = 999.99,
+                Duration = duration,
+            };
+            
+            data.Add(newDataPoint);
+            pointTime = newDataPoint.Time + duration;
+        }
+        
+        var json = new 
+        {
+            Emissions = data
+        };
+
+        File.WriteAllText(path, JsonSerializer.Serialize(json));
+    }
     public void SetupForecastMock() { }
     public void Initialize() { }
     public void Reset() { }
