4dd47c76edc10d6ddac898b39bfdad83e4baa360
Priti <pritipathak@microsoft.com>
2022-11-22 10:35:28 -0500

    [M2][#174] ElectricityMaps Data Source - Emissions

--- src/CarbonAware.DataSources/CarbonAware.DataSources.ElectricityMaps/src/Constants/QueryStrings.cs
+++ src/CarbonAware.DataSources/CarbonAware.DataSources.ElectricityMaps/src/Constants/QueryStrings.cs
@@ -5,4 +5,8 @@ internal class QueryStrings
     public const string Latitude = "lat";
     public const string Longitude = "lon";
     public const string ZoneName = "zone";
+    public const string StartTime = "start";
+    public const string EndTime = "end";
+    public const string DisableEstimations = "disableEstimations";
+    public const string EmissionFactorType = "emissionFactorType";
 }
