aa3a100bc6c0ae926f00a2b71d018e1b48eb034b
Priti <pritipathak@microsoft.com>
2023-01-05 10:53:38 -0500

    Review comments

--- src/CarbonAware/test/model/LocationTests.cs
+++ src/CarbonAware/test/model/LocationTests.cs
@@ -10,29 +10,14 @@ namespace CarbonAware.Tests.model;
 
 public class LocationTests
 {
-    private static Location _defaultLocation = new Location() { Name = "eastus", Latitude = 34.123m, Longitude = 123.456m };
-
-    [Test]
-    public void LatLongConversionToInvariantCulture_WhenThreadCultureSpecified()
+    [TestCase(34.123, "34.123", "en-US", TestName = "Lat/Long conversion to culture invariant string for en-US")]
+    [TestCase(34.123, "34.123", "da-DK", TestName = "Lat/Long conversion to culture invariant string for da-DK")]
+    public void LatLongConversionToInvariantCulture_WhenThreadCultureSpecified(decimal value, string expectedValue , string culture)
     {
-        var expectedLatitude = "34.123";
-        var expectedLongitude = "123.456";
+        Thread.CurrentThread.CurrentCulture = new CultureInfo(culture);
+        Location _defaultLocation = new Location() { Name = "eastus", Latitude = value, Longitude = value};
 
-        Thread.CurrentThread.CurrentCulture = new CultureInfo("da-DK");
-
-        Assert.That(_defaultLocation.LatitudeAsCultureInvariantString(), Is.EqualTo(expectedLatitude));
-        Assert.That(_defaultLocation.LongitudeAsCultureInvariantString(), Is.EqualTo(expectedLongitude));
-    }
-
-    [Test]
-    public void LatLongConversionToString_WhenThreadCultureSpecified()
-    {
-        var expectedLatitude = "34,123";
-        var expectedLongitude = "123,456";
-
-        Thread.CurrentThread.CurrentCulture = new CultureInfo("da-DK");
-
-        Assert.That(_defaultLocation.Latitude.ToString(), Is.EqualTo(expectedLatitude));
-        Assert.That(_defaultLocation.Longitude.ToString(), Is.EqualTo(expectedLongitude));
+        Assert.That(_defaultLocation.LatitudeAsCultureInvariantString(), Is.EqualTo(expectedValue));
+        Assert.That(_defaultLocation.LongitudeAsCultureInvariantString(), Is.EqualTo(expectedValue));
     }
 }
