4e772dd7323bdf04905252a8a3c6a9e3c9deb2db
Margaret <in4margaret@gmail.com>
2023-01-30 10:00:26 -0500

    Change aggregator to handler

--- src/CarbonAware.CLI/test/integrationTests/Commands/Emissions/EmissionsCommandTests.cs
+++ src/CarbonAware.CLI/test/integrationTests/Commands/Emissions/EmissionsCommandTests.cs
@@ -6,7 +6,7 @@ using System.Text.RegularExpressions;
 namespace CarbonAware.CLI.IntegrationTests.Commands.Emissions;
 
 /// <summary>
-/// Tests that the CLI handles and packages various responses from aggregators 
+/// Tests that the CLI handles and packages various responses from handlers 
 /// and data sources properly, including empty responses and exceptions.
 /// </summary>
 [TestFixture(DataSourceType.JSON)]
