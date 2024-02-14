3b8abfa40b1e1e077765efb6a1bd011d9f6698a7
Osama Jandali <osamajandaly@gmail.com>
2024-02-14 16:00:15 +0200

    Update baseURL docusaurus config
    Signed-off-by: Osama Jandali <osamajandaly@gmail.com>

--- casdk-docs/docusaurus.config.js
+++ casdk-docs/docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://green-software-foundation.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/carbon-aware-sdk',
+  baseUrl: '/',
    // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'Green-Software-Foundation', // Usually your GitHub org/user name.
