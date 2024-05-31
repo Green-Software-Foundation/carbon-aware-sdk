ab1205d699f99887e9a7e59f2bf8b0a692690df6
Vaughan Knight <vaughan@vaughanknight.com>
2024-05-31 16:55:18 +0100

    Updated start and end configuration
    Updated start and end configuration to new values

--- src/CarbonAware.DataSources/CarbonAware.DataSources.WattTime/src/Constants/QueryStrings.cs
+++ src/CarbonAware.DataSources/CarbonAware.DataSources.WattTime/src/Constants/QueryStrings.cs
@@ -3,8 +3,8 @@
 internal class QueryStrings
 {
     public const string BalancingAuthorityAbbreviation = "ba";
-    public const string StartTime = "starttime";
-    public const string EndTime = "endtime";
+    public const string StartTime = "start";
+    public const string EndTime = "end";
     public const string Latitude = "latitude";
     public const string Longitude = "longitude";
     public const string Username = "username";
