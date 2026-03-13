6d360b0677d9882ae15fe26952d56c377984426c
xginn8 <xginn8@users.noreply.github.com>
2026-03-13 12:04:35 -0400

    Update WattTime sample API endpoints and request parameters
    WattTime API access has changed since the initial implementation
    
    Signed-off-by: xginn8 <xginn8@users.noreply.github.com>

--- samples/watttime-registration/3-makerequest.py
+++ samples/watttime-registration/3-makerequest.py
@@ -3,18 +3,19 @@ from requests.auth import HTTPBasicAuth
 from settings import *
 from datetime import date
 
-login_url = 'https://api2.watttime.org/v2/login'
+login_url = 'https://api.watttime.org/login'
 token = requests.get(login_url, auth=HTTPBasicAuth(wt_username, wt_password)).json()['token']
 
-data_url = 'https://api2.watttime.org/v2/data'
+data_url = 'https://api.watttime.org/v3/historical'
 headers = {'Authorization': 'Bearer {}'.format(token)}
 
 last_year = str(date.today().year - 1)
 starttime = last_year + '-02-20T16:00:00-0800'
 endtime   = last_year + '-02-20T16:15:00-0800'
 
-params = {'ba': 'CAISO_NORTH', 
-          'starttime': starttime, 
-          'endtime': endtime}
+params = {'region': 'CAISO_NORTH', 
+          'signal_type': 'co2_moer',
+          'start': starttime, 
+          'end': endtime}
 rsp = requests.get(data_url, headers=headers, params=params)
 print(rsp.text)
